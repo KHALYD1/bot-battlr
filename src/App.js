eb04bb7ed6689f4f22b0b18ce79d146f82b0c494
Khalid <khaliddakar001@gmail.com>
2023-05-02 10:57:37 +0300

    linked app.js to chakraui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heading, VStack } from "@chakra-ui/react";
+
+import { TransactionList } from "./component/TransactionList";
+
 
 function App() {
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <container>
+      <VStack>
+        <Heading>Bank Of Flatiron</Heading>
+        <div>Form</div>
+        <TransactionList />
+        
+      </VStack>
+    </container>
+  
   );
 }
+
 
 export default App;
